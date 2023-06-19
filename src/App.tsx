8eb4627c8b014a3b2ba321cc3ef0557205dbf3c9
Sebastien Stordeur <stordeursebastien0@gmail.com>
2023-06-19 10:35:29 +0200

    Custom select component with logic in it

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Map from "./components/Map/Map";
+import List from "./components/UI/List/List";
 import "./styles/style.css";
 
 function App() {
   return (
     <main id="main" className="main-container">
+      <List />
       <section id="map-container" className="map-container">
         <Map />
       </section>
--- src/components/UI/List/List.tsx
+++ src/components/UI/List/List.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { FC, useEffect, useRef } from "react";
+import { useState } from "react";
 
-const List = () => {
-  return <div>List</div>;
+const List: FC = () => {
+  const [isOpen, setisOpen] = useState(false);
+  const [optionSelected, setOptionSelected] = useState<null | string>(null);
+
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+
+  const options = [
+    { label: "Option 1", value: "option1" },
+    { label: "Option 2", value: "option2" },
+    { label: "Option 3", value: "option3" },
+  ];
+
+  const toggleList = () => setisOpen(!isOpen);
+  const handleSelect = (value: string) => {
+    setOptionSelected(value);
+    toggleList();
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        toggleList();
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
+  return (
+    <div className="custom-list" ref={dropdownRef}>
+      <div onClick={toggleList}>{optionSelected ? optionSelected : "Select an option"}</div>
+      {isOpen && (
+        <div>
+          {options.map((option) => (
+            <div key={option.value} onClick={() => handleSelect(option.value)} className="option">
+              {option.value}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default List;
